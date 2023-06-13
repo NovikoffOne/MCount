1135a18797e0c8e7afd9714993ec1574090372f8
NovikoffOne <104835127+NovikoffOne@users.noreply.github.com>
2023-06-13 17:17:40 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace MCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] counts = new int[] { 
+                4, 4, 4, 4, 1, 4, 4, 4, 5, 4, 5, 6, 6, 6, 6,
+                4, 4, 4, 2, 2, 2, 2, 2, 5, 5, 5, 5, 5, 5, 2 
+            };
+
+            int maximumRepetitions;
+            int templateRepetitions;
+            int numberWithMaximumRepetition;
+
+            maximumRepetitions = 0;
+            templateRepetitions = 0;
+            numberWithMaximumRepetition = 0;
+
+            for (int i = 0; i < counts.Length; i++)
+            {
+                if (i != 0)
+                {
+                    if (counts[i] == counts[i - 1])
+                    {
+                        ++templateRepetitions;
+
+                        if (templateRepetitions > maximumRepetitions)
+                        {
+                            maximumRepetitions = templateRepetitions;
+                            numberWithMaximumRepetition = counts[i];
+                        }
+                    }
+                    else
+                    {
+                        templateRepetitions = 0;
+                        ++templateRepetitions;
+                    }
+                }
+                else
+                {
+                    templateRepetitions++;
+                }
+            }
+
+            string arrayToText = string.Join(",", counts);
+
+            Console.WriteLine($"{arrayToText} - число {numberWithMaximumRepetition} повторяется {maximumRepetitions} раз.");
+        }
+    }
+}
