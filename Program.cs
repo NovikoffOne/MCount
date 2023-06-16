3761ad729c4f2003228056a4ffae1322700d0b4b
NovikoffOne <104835127+NovikoffOne@users.noreply.github.com>
2023-06-16 08:55:39 +0300

    Commit 1

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@ namespace MCount
     {
         static void Main(string[] args)
         {
-            int[] counts = new int[] { 
+            int[] counts = new int[] {
                 4, 4, 4, 4, 1, 4, 4, 4, 5, 4, 5, 6, 6, 6, 6,
-                4, 4, 4, 2, 2, 2, 2, 2, 5, 5, 5, 5, 5, 5, 2 
+                4, 4, 4, 2, 2, 2, 2, 2, 5, 5, 5, 5, 5, 5, 2
             };
 
             int maximumRepetitions;
@@ -16,32 +16,25 @@ namespace MCount
             int numberWithMaximumRepetition;
 
             maximumRepetitions = 0;
-            templateRepetitions = 0;
+            templateRepetitions = 1;
             numberWithMaximumRepetition = 0;
 
-            for (int i = 0; i < counts.Length; i++)
+            for (int i = 1; i < counts.Length; i++)
             {
-                if (i != 0)
+                if (counts[i] == counts[i - 1])
                 {
-                    if (counts[i] == counts[i - 1])
-                    {
-                        ++templateRepetitions;
+                    ++templateRepetitions;
 
-                        if (templateRepetitions > maximumRepetitions)
-                        {
-                            maximumRepetitions = templateRepetitions;
-                            numberWithMaximumRepetition = counts[i];
-                        }
-                    }
-                    else
+                    if (templateRepetitions > maximumRepetitions)
                     {
-                        templateRepetitions = 0;
-                        ++templateRepetitions;
+                        maximumRepetitions = templateRepetitions;
+                        numberWithMaximumRepetition = counts[i];
                     }
                 }
                 else
                 {
-                    templateRepetitions++;
+                    templateRepetitions = 0;
+                    ++templateRepetitions;
                 }
             }
 
